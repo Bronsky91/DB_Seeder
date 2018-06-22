f841ab34779e63b8feb1bba6a6fcced2ee6a7269
Bryan <bryan87reed@gmail.com>
2018-06-21 17:38:21 -0700

    Adds planning comments to random_gen

--- random_gen.py
+++ random_gen.py
@@ -1,1 +1,16 @@
 # File that will hold functions that create and pull random data values
+# http://namey.muffinlabs.com/api.js
+# Import this file directly in Redtail.py file
+
+# Create random Contact data
+#   use random surnames to create street addresses
+
+# Create random calendar data
+#   subjects (Call Backs, Reviews, etc)
+
+# Create random Account data
+#   add data check for contact records in Redtail file, extra api call before put calls
+
+# Create random Seminar data
+
+# Create random Opp data
